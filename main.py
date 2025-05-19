2ec9e631f18077dea7b153f8f5a862fcb8f95ae6
yindraad <xlefr7@gmail.com>
2025-05-18 22:07:04 -0700

    Python3 Asteroid Game Bootdev

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    print(f"Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
